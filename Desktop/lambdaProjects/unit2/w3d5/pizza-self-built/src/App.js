6deefb908c6f6a66411c39129f778094628c8c32
Max Huckstepp <mhuckstepp@gmail.com>
2021-02-26 08:45:43 -0800

    added routing

--- Desktop/lambdaProjects/unit2/w3d5/pizza-self-built/src/App.js
+++ Desktop/lambdaProjects/unit2/w3d5/pizza-self-built/src/App.js
@@ -1,10 +1,20 @@
+import { Route } from "react-router-dom";
 import React from "react";
+import Home from "./components/Home";
+import PizzaForm from "./components/PizzaForm";
+import SuccessPage from "./components/SuccessPage";
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <header>
+        <h3>Lambda Eats</h3>
+        <button>Home</button>
+        <button>Help</button>
+      </header>
+      <Route exact path="/" component={Home} />
+      <Route path="/pizza" component={PizzaForm} />
+      <Route path="/success" component={SuccessPage} />
     </>
   );
 };
---
+++ Desktop/lambdaProjects/unit2/w3d5/pizza-self-built/src/components/PizzaForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PizzaForm = () => {
+  return (
+    <div>
+      <h1>PIZZA</h1>
+    </div>
+  );
+};
+
+export default PizzaForm;
