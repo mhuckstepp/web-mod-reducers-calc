77955219c1921d4b4ddc693321d7c774482534fd
Max Huckstepp <mhuckstepp@gmail.com>
2021-03-13 18:39:26 -0800

    project complete

---
+++ Desktop/lambdaProjects/unit3/Week2/new-calc-self-built/src/App.js
@@ -0,0 +1,92 @@
+import React from "react";
+import "./App.css";
+import TotalDisplay from "./components/TotalDisplay";
+import CalcButton from "./components/CalcButton";
+import reducer, { initialState } from "./reducers";
+import { useReducer } from "react";
+import {
+  changeOperation,
+  applyNumber,
+  clearNum,
+  setMem,
+  memTotal,
+  memClear,
+} from "./actions";
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="App">
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">
+          <img width="40px" src="./Lambda-Logo-Red.png" alt="pic" /> Lambda
+          Reducer Challenge
+        </a>
+      </nav>
+
+      <div className="container row mt-5">
+        <div className="col-md-12 d-flex justify-content-center">
+          <form name="Cal">
+            <TotalDisplay value={state.total} />
+            <div className="row details">
+              <span id="operation">
+                <b>Operation:</b> {state.operation}
+              </span>
+              <span id="memory">
+                <b>Memory:</b> {state.memory}
+              </span>
+            </div>
+
+            <div className="row">
+              <CalcButton value={"M+"} onClick={() => dispatch(setMem())} />
+              <CalcButton value={"MR"} onClick={() => dispatch(memTotal())} />
+              <CalcButton value={"MC"} onClick={() => dispatch(memClear())} />
+            </div>
+
+            <div className="row">
+              <CalcButton value={1} onClick={() => dispatch(applyNumber(1))} />
+              <CalcButton value={2} onClick={() => dispatch(applyNumber(2))} />
+              <CalcButton value={3} onClick={() => dispatch(applyNumber(3))} />
+            </div>
+
+            <div className="row">
+              <CalcButton value={4} onClick={() => dispatch(applyNumber(4))} />
+              <CalcButton value={5} onClick={() => dispatch(applyNumber(5))} />
+              <CalcButton value={6} onClick={() => dispatch(applyNumber(6))} />
+            </div>
+
+            <div className="row">
+              <CalcButton value={7} onClick={() => dispatch(applyNumber(7))} />
+              <CalcButton value={8} onClick={() => dispatch(applyNumber(8))} />
+              <CalcButton value={9} onClick={() => dispatch(applyNumber(9))} />
+            </div>
+
+            <div className="row">
+              <CalcButton
+                value={"+"}
+                onClick={() => {
+                  dispatch(changeOperation("+"));
+                }}
+              />
+              <CalcButton
+                value={"*"}
+                onClick={() => dispatch(changeOperation("*"))}
+              />
+              <CalcButton
+                value={"-"}
+                onClick={() => dispatch(changeOperation("-"))}
+              />
+            </div>
+
+            <div className="row ce_button">
+              <CalcButton value={"CE"} onClick={() => dispatch(clearNum())} />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
